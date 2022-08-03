f9f205e97473da315dbc10d64cc7b3c4a2e5df8c
limseongmuk <kmkook12312@gmail.com>
2022-08-03 19:00:27 +0900

    refactor (Package) : Adjust folder location

--- src/main/java/com/levelUpToast/levelUpToast/domain/login/loginServiceImpl/SimpleLoginService.java
+++
@@ -1,7 +0,0 @@
-package com.levelUpToast.levelUpToast.domain.login.loginServiceImpl;
-
-import com.levelUpToast.levelUpToast.domain.login.loginServiceInterface.LoginService;
-
-public class SimpleLoginService implements LoginService {
-
-}
