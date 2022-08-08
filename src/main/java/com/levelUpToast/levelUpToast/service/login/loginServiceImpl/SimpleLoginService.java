3662c147be1658f847ad7147d2889f05bf9ce961
JiYongKim <colorful8315@gmail.com>
2022-08-08 23:39:23 +0900

    refactor (LoginService) : Refactoring LoginService & SimpleLoginService

--- src/main/java/com/levelUpToast/levelUpToast/service/login/loginServiceImpl/SimpleLoginService.java
+++ src/main/java/com/levelUpToast/levelUpToast/service/login/loginServiceImpl/SimpleLoginService.java
@@ -1,7 +1,19 @@
 package com.levelUpToast.levelUpToast.service.login.loginServiceImpl;
 
+import com.levelUpToast.levelUpToast.domain.member.Member;
+import com.levelUpToast.levelUpToast.domain.repository.memberRepository.memberRepositoryInf.MemberRepository;
 import com.levelUpToast.levelUpToast.service.login.loginServiceInterface.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service @AllArgsConstructor
 public class SimpleLoginService implements LoginService {
 
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member login(String loginId, String password) {
+
+        return null;
+    }
 }
--- src/main/java/com/levelUpToast/levelUpToast/service/login/loginServiceInterface/LoginService.java
+++ src/main/java/com/levelUpToast/levelUpToast/service/login/loginServiceInterface/LoginService.java
@@ -1,4 +1,7 @@
 package com.levelUpToast.levelUpToast.service.login.loginServiceInterface;
 
+import com.levelUpToast.levelUpToast.domain.member.Member;
+
 public interface LoginService {
+    Member login(String loginId, String password);
 }
