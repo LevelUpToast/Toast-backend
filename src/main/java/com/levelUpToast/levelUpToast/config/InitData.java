87d98bd5aeed052c817164a65275e41e5c5ecb21
JiYongKim <colorful8315@gmail.com>
2022-08-10 23:07:53 +0900

    chore (initData) :  refactor initData Member name

--- src/main/java/com/levelUpToast/levelUpToast/config/InitData.java
+++ src/main/java/com/levelUpToast/levelUpToast/config/InitData.java
@@ -17,8 +17,8 @@ public class InitData {
         //init Member
         memberRepository.save(new Member("ji", "ji", Authority.ADMIN, "김지용", "970128", "남", "010-6277-0650", "colorful8315@gmail.com", "seoul"));
         memberRepository.save(new Member("mook", "mook", Authority.ADMIN, "임성묵", "000000", "남", "---", "---", "seoul"));
-        memberRepository.save(new Member("beom", "beom", Authority.ADMIN, "임성묵", "000000", "남", "---", "---", "seoul"));
-        memberRepository.save(new Member("saac", "saac", Authority.ADMIN, "임성묵", "000000", "남", "---", "---", "seoul"));
+        memberRepository.save(new Member("beom", "beom", Authority.ADMIN, "김준범", "000000", "남", "---", "---", "seoul"));
+        memberRepository.save(new Member("saac", "saac", Authority.ADMIN, "이삭", "000000", "남", "---", "---", "seoul"));
 
 
     }
