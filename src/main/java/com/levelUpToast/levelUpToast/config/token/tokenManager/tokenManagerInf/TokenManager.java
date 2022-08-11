fd0845f1acc38b82eea87fafa8d5cab8b76dccb2
JiYongKim <colorful8315@gmail.com>
2022-08-11 09:51:29 +0900

    refactor (logout) :  Add logout Service

--- src/main/java/com/levelUpToast/levelUpToast/config/token/tokenManager/tokenManagerInf/TokenManager.java
+++ src/main/java/com/levelUpToast/levelUpToast/config/token/tokenManager/tokenManagerInf/TokenManager.java
@@ -1,5 +1,6 @@
 package com.levelUpToast.levelUpToast.config.token.tokenManager.tokenManagerInf;
 
+import com.levelUpToast.levelUpToast.config.exception.LevelUpToastEx;
 import com.levelUpToast.levelUpToast.domain.member.Member;
 
 public interface TokenManager {
@@ -8,11 +9,11 @@ public interface TokenManager {
     String createToken(Long manageSeq);
 
     //token 조회 (로그인 완료 사용자 데이터 조회)
-    Member findMemberByToken(String token) throws Exception;
+    Member findMemberByToken(String token) throws LevelUpToastEx;
 
     //token 만료 조회 (로그인 완료 확인용)
     Boolean tokenCheck(String token);
 
     //token 만료
-    void tokenExpire(String token) throws Exception;
+    void tokenExpire(String token) throws LevelUpToastEx;
 }
--- src/main/java/com/levelUpToast/levelUpToast/rest/login/LoginController.java
+++ src/main/java/com/levelUpToast/levelUpToast/rest/login/LoginController.java
@@ -8,9 +8,7 @@ import com.levelUpToast.levelUpToast.domain.member.Member;
 import com.levelUpToast.levelUpToast.service.login.loginServiceInterface.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -20,21 +18,28 @@ import java.util.HashMap;
 public class LoginController {
 
     private final LoginService loginService;
-    private final TokenManager tokenManager;
-
 
     @PostMapping("/login")
     public LoginResponseForm login(@RequestBody LoginRequestFrom form) {
         HashMap<String, String> data = new HashMap<>();
         try {
             Member loginMem = loginService.login(form.getId(), form.getPw());
-            String token = tokenManager.createToken(loginMem.getManageSeq());
-            data.put("token", token);
+            data.put("token", loginMem.getToken());
             log.info("[LoginController log] : 로그인 성공 manageSeq = {}, loginId = {}, name = {}",loginMem.getManageSeq(),loginMem.getId(),loginMem.getName());
             return new LoginResponseForm(-1,"로그인 성공",data);
         } catch (LevelUpToastEx e) {
             data.put("token", null);
             return new LoginResponseForm(e.getERR_CODE(),e.getMessage(),data);
         }
+    }
+
+    @GetMapping("/logout/{token}")
+    public LoginResponseForm logout(@PathVariable String token) {
+        try {
+            loginService.logout(token);
+        } catch (LevelUpToastEx e) {
+            return new LoginResponseForm(e.getERR_CODE(), e.getMessage(), null);
+        }
+        return new LoginResponseForm(-1, "로그아웃 성공", null);
     }
 }
