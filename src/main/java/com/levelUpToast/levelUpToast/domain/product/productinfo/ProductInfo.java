9ce997cd92f40d3c5271f7596242e9339e01b72c
limseongmuk <kmkook12312@gmail.com>
2022-08-17 18:48:15 +0900

    feat (ProductInfo) : Product ProductInfo.java make

---
+++ src/main/java/com/levelUpToast/levelUpToast/domain/product/productinfo/ProductInfo.java
@@ -0,0 +1,11 @@
+package com.levelUpToast.levelUpToast.domain.product.productinfo;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class ProductInfo {
+    private String text;
+    private ArrayList<String> imgUrl;
+}
