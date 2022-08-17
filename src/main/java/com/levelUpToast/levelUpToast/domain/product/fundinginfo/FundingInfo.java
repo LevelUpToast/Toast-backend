c9def986ab77c79e2d319d491af6767b2f0b717b
limseongmuk <kmkook12312@gmail.com>
2022-08-17 18:47:31 +0900

    feat (FundingInfo) : Product FundingInfo.java make

---
+++ src/main/java/com/levelUpToast/levelUpToast/domain/product/fundinginfo/FundingInfo.java
@@ -0,0 +1,11 @@
+package com.levelUpToast.levelUpToast.domain.product.fundinginfo;
+
+import lombok.Data;
+
+@Data
+public class FundingInfo {
+    private int currentAmount;
+    private int finalAmount;
+    private String deadline;
+
+}
