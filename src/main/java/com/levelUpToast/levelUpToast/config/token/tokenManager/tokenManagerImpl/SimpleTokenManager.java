047efacec75a69336e3d97511cb3b64cb584d59a
JiYongKim <colorful8315@gmail.com>
2022-08-28 20:12:54 +0900

    chore (tokenManager) :  change tokenmanager return type

--- src/main/java/com/levelUpToast/levelUpToast/config/token/tokenManager/tokenManagerImpl/SimpleTokenManager.java
+++ src/main/java/com/levelUpToast/levelUpToast/config/token/tokenManager/tokenManagerImpl/SimpleTokenManager.java
@@ -2,21 +2,17 @@ package com.levelUpToast.levelUpToast.config.token.tokenManager.tokenManagerImpl
 
 import com.levelUpToast.levelUpToast.config.exception.LevelUpToastEx;
 import com.levelUpToast.levelUpToast.config.token.tokenManager.tokenManagerInf.TokenManager;
-import com.levelUpToast.levelUpToast.domain.member.Member;
-import com.levelUpToast.levelUpToast.domain.repository.memberRepository.memberRepositoryInf.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SimpleTokenManager implements TokenManager {
-    private final MemberRepository memberRepository;
     private Map<String, Long> tokenStore = new ConcurrentHashMap<>();
 
     @Override
@@ -27,18 +23,12 @@ public class SimpleTokenManager implements TokenManager {
     }
 
     @Override
-    public Member findMemberByToken(String token) throws LevelUpToastEx {
+    public Long findMemberSeqByToken(String token) throws LevelUpToastEx {
        if(!tokenCheck(token)){
            log.warn("[TokenManager log] : 존재하지 않는 토큰 token = {}",token);
            throw new LevelUpToastEx("존재하지 않는 토큰 입니다.",3);
        }
-
-       Optional<Member> findMem = memberRepository.findByManageSeq(tokenStore.get(token));
-       if(findMem.isEmpty()){
-           log.warn("[TokenManager log] : 토큰과 일치하는 사용자 미존재 token = {}",token);
-           throw new LevelUpToastEx("토큰과 일치하는 사용자가 존재하지 않습니다.",4);
-       }
-       return findMem.get();
+        return tokenStore.get(token);
     }
 
     @Override
