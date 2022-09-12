1ab910977828f60f9c730be125dd4ccc10be9ada
limseongmuk <kmkook12312@gmail.com>
2022-09-12 18:34:13 +0900

    fix (Product) : Fix code bugs

--- src/main/java/com/levelUpToast/levelUpToast/domain/repository/productRepository/productRepositoryImpl/MemoryProductRepository.java
+++ src/main/java/com/levelUpToast/levelUpToast/domain/repository/productRepository/productRepositoryImpl/MemoryProductRepository.java
@@ -27,9 +27,9 @@ public class MemoryProductRepository implements ProductRepository {
     }
 
     @Override
-    public Product updateProduct(Long productSeq, Product newProduct) {
-        if (productStore.containsKey(productSeq)) productStore.put(productSeq, newProduct);
-        return null;
+    public void updateProduct(Long productSeq, Product newProduct) {
+        if (productStore.containsKey(productSeq))
+            productStore.put(productSeq, newProduct);
     }
 
     @Override
